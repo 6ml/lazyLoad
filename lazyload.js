f26e56a5bb9f4fce6c6c63ecf433b9e74f8981bd
lupath <849104822@qq.com>
2017-08-18 16:49:52 +0800

    chore: add a method to get el's offsetTop

--- lazyload.js
+++ lazyload.js
@@ -1,21 +1,17 @@
 window.onload = function () {
-    if(document.addEventListener) {
-        window.addEventListener('scroll', throttle(judgeShow, 500, 1000), false);
-    } else {
-        window.attachEvent('onscroll', throttle(judgeShow, 500, 1000));
-    }
+    addEvent(window, 'scroll', throttle(judgeShow, 500, 1000), false);
 };
 
 /**
- * 获取元素 offsetTop 值
- * @param  {[Object]} obj [要获取 offsetTop 值的元素]
+ * 获取元素 offsetTop 值  -->  通过循环累加元素相对父节点 offsetTop 值
+ * @param  {[Object]} el  [要获取 offsetTop 值的元素]
  * @return {[Number]}     [元素 offsetTop 值]
  */
-function getOffsetTop(obj) {
+function getOffsetTop1(el) {
     'use strict';
 
-    var offsetTop = obj.offsetTop;
-    var parent = obj.offsetParent;
+    var offsetTop = el.offsetTop;
+    var parent = el.offsetParent;
 
     while(parent) {
         offsetTop += parent.offsetTop;
@@ -26,17 +22,37 @@ function getOffsetTop(obj) {
 }
 
 /**
+ * 获取元素 offsetTop 值  -->  通过 getBoundingClientRect 方法
+ * @param  {[type]} el [description]
+ * @return {[type]}    [description]
+ */
+function getOffsetTop2(el) {
+    'use strict';
+
+    // 浏览器滚动高度
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+    // el.getBoundingClientRect().top 获取到元素到浏览器视窗顶部的距离
+    return el.getBoundingClientRect().top + scrollTop;
+}
+
+/**
  * 判断元素是否出现是视野范围内
  */
 function judgeShow() {
     'use strict';
 
+    // 获取浏览器滚动高度
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    // 浏览器视窗高度
     var clientHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
     var imgList = getElementsByClass('lazyload', 'img');
     imgList.forEach(function (imgItem) {
-        if(getOffsetTop(imgItem) - scrollTop <= clientHeight) {
+
+        // document.documentElement.clientTop 在 IE 6/7 中值为 2 ; 在其他浏览器中值为 0
+        if(getOffsetTop1(imgItem) - document.documentElement.clientTop - scrollTop <= clientHeight) {
+        // if(getOffsetTop2(imgItem) - document.documentElement.clientTop - scrollTop <= clientHeight) {
             setSrc(imgItem);
         }
     });
@@ -60,7 +76,7 @@ function setSrc(obj) {
 }
 
 /**
- * 根据类名查找图片元素
+ * 根据类名和标签名查找元素
  * @param  {[String]}   className   [要查找元素的类名]
  * @param  {[String]}   tagName     [要查找元素的标签名]
  * @return {[Array]}                [查找到的元素集合]
@@ -84,6 +100,22 @@ function getElementsByClass(className, tagName) {
     else{
         return Array.prototype.slice.call(document.getElementsByClassName(className), 0);
     }
+}
+
+/**
+ * 绑定事件
+ * @param {[Object]}   	el     [要绑定事件的元素]
+ * @param {[String]}   	method [要绑定的事件名]
+ * @param {Function} 	fn     [事件处理函数]
+ * @param {[Boolean]}   bool   [事件捕获阶段触发还是事件冒泡阶段触发]
+ */
+function addEvent (el, method, fn, bool) {
+	if(document.addEventListener) {
+		el.addEventListener(method, fn, bool);
+	}
+	else {
+		el.attachEvent('on' + method, fn);
+	}
 }
 
 /**
